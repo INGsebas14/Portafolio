9429f00cf8c216fb53decbd2ff2c74b86fb659d9
INGsebas14 <150859797+INGsebas14@users.noreply.github.com>
2024-07-14 13:24:44 -0500

    Inicio de portafolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function instagram() {
+    window.location.href="https://www.instagram.com/sebas.05_14/";
+}
+function gitHub() {
+    window.location.href="https://github.com/INGsebas14";
+}
+function linkedin() {
+    window.location.href="https://www.linkedin.com/in/sebastian-fonseca-matta/";
+}
